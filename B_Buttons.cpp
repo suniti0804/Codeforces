6bb95dd34b0a7022766a0babd5843af2b4c94a0a
Suniti <suniti0804@gmail.com>
2022-02-24 11:05:20 +0530

    code

---
+++ B_Buttons.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define f(i, a, b) for(int i=a; i<b; i++)
+#define ll long long 
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        
+    }
+    return 0;
+}
---
+++ B_Present_from_Lena.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define f(i, a, b) for(int i=a; i<b; i++)
+#define ll long long 
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n;
+    cin>>n;
+
+    for(int i=0; i<2*n; i++)
+    {
+        for(int j=0; j<2*n; j++)
+        {
+            
+        }
+    }
+    return 0;
+}
